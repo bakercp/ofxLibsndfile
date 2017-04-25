3a159ccc85193b3ae705b15841a130c23e68ce22
Christopher Baker <me@christopherbaker.net>
2017-04-25 10:04:11 -0500

    Add addons_config.mk

---
+++ addon_config.mk
@@ -0,0 +1,9 @@
+meta:
+	ADDON_NAME = ofxLibsndfile
+	ADDON_DESCRIPTION = An libsndfile wrapper.
+	ADDON_AUTHOR = Christopher Baker
+	ADDON_TAGS = "sound" "sound files" "audio" "audio files"
+	ADDON_URL = http://github.com/bakercp/ofxLibsndfile
+
+linux:
+	ADDON_PKG_CONFIG_LIBRARIES = libsndfile
